dd0cf434ec6ad7a9b370208f230ddda9adaf3527
Purplexionist <eggertwilliar@gmail.com>
2018-06-03 11:36:52 -0700

    bangi

--- EvaluateCFRandom.py
+++ EvaluateCFRandom.py
@@ -4,15 +4,14 @@ from random import randint
 import statistics as s
 import time
 
-def pearson(row1, row2, row1Avg, row2Avg):
+def pearson(row1, row2, row1Avg, row2Avg, finSet):
 	top = 0
 	bottomRow1 = 0
 	bottomRow2 = 0
-	for i in range(len(row1)):
-		if row1[i] != 99 and row2[i] != 99:
-			top += (row1[i] - row1Avg) * (row2[i] - row2Avg)
-			bottomRow1 += (row1[i] - row1Avg) ** 2
-			bottomRow2 += (row2[i] - row2Avg) ** 2
+	for i in list(finSet)
+		top += (row1[i] - row1Avg) * (row2[i] - row2Avg)
+		bottomRow1 += (row1[i] - row1Avg) ** 2
+		bottomRow2 += (row2[i] - row2Avg) ** 2
 	return top/((bottomRow1 ** .5) * (bottomRow2 ** .5))
 
 #i use the transpose matrix for item based
@@ -21,7 +20,7 @@ def meanUser(userScores, avg, numRated, first, second):
 	temp -= userScores[first][second]
 	return temp/(numRated - 1)
 
-def weightedUser(userScores, avgForIndividual, countForIndividual, first, second, allAvg, allCount):
+def weightedUser(userScores, avgForIndividual, countForIndividual, first, second, allAvg, allCount, allSets):
 	tot = 0
 	kTot = 0
 	actualScore = userScores[first][second]
@@ -32,7 +31,9 @@ def weightedUser(userScores, avgForIndividual, countForIndividual, first, second
 		row = userScores[i]
 		if row[second] != 99:
 			theirAvg = allAvg[i]
-			sim = pearson(userRow, row, userAvg, theirAvg)
+			finSet = allSets[first].intersection(allSets[i])
+			finSet.remove(second)
+			sim = pearson(userRow, row, userAvg, theirAvg, finSet)
 			kTot += abs(sim)
 			tot += sim * row[second]
 	userScores[first][second] = actualScore
@@ -51,6 +52,8 @@ def main():
 	itemsRatedPerItem = [0 for i in range(100)]
 	avgPerUser = [0 for i in range(24983)]
 	avgPerItem = [0 for i in range(100)]
+	userSet = [set() for i in range(24983)]
+	itemSet = [set() for i in range(100)]
 
 	i = 0
 	for line in f:
@@ -64,8 +67,11 @@ def main():
 				itemsRatedPerItem[j] += 1
 				avgPerUser[i] += score
 				avgPerItem[j] += score
+				userSet[i].add(j)
+				itemSet[j].add(i)
 			j += 1
 		i += 1 
+
 
 	for i in range(24983):
 		avgPerUser[i] /= itemsRatedPerUser[i]
@@ -74,6 +80,7 @@ def main():
 		avgPerItem[i] /= itemsRatedPerItem[i]
 
 	itemScores = np.transpose(userScores)
+
 	#generate random  user item tuples
 	#then perform method and find MAE
 	MAEList = []
@@ -103,14 +110,14 @@ def main():
 			for tup in tupSet:
 				actual = userScores[tup[0]][tup[1]]
 				pred = weightedUser(userScores, avgPerUser[tup[0]], itemsRatedPerUser[tup[0]], tup[0], tup[1],
-					avgPerUser, itemsRatedPerUser)
+					avgPerUser, itemsRatedPerUser, userSet)
 				MAE += abs(actual - pred)
 				count += 1
 		if method == "weightedItem":
 			for tup in tupSet:
 				actual = itemScores[tup[1]][tup[0]]
 				pred = weightedUser(itemScores, avgPerItem[tup[1]], itemsRatedPerItem[tup[1]], tup[1], tup[0],
-					avgPerItem, itemsRatedPerItem)
+					avgPerItem, itemsRatedPerItem, itemSet)
 				MAE += abs(actual - pred)
 				count += 1
 
