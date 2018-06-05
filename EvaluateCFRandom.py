181d6d8f14532a820d9a6314c365ca5b842f3df4
Purplexionist <eggertwilliar@gmail.com>
2018-06-05 13:25:09 -0700

    finished first program

--- EvaluateCFRandom.py
+++ EvaluateCFRandom.py
@@ -8,7 +8,7 @@ def pearson(row1, row2, row1Avg, row2Avg, finSet):
 	top = 0
 	bottomRow1 = 0
 	bottomRow2 = 0
-	for i in list(finSet)
+	for i in list(finSet):
 		top += (row1[i] - row1Avg) * (row2[i] - row2Avg)
 		bottomRow1 += (row1[i] - row1Avg) ** 2
 		bottomRow2 += (row2[i] - row2Avg) ** 2
@@ -39,13 +39,46 @@ def weightedUser(userScores, avgForIndividual, countForIndividual, first, second
 	userScores[first][second] = actualScore
 	return tot/kTot
 
+def knnUser(userScores, avgForIndividual, countForIndividual, first, second, allAvg, allCount, allSets, knnNum):
+	tot = 0
+	kTot = 0
+	simList = []
+	actualScore = userScores[first][second]
+	userScores[first][second] = 99
+	userRow = userScores[first]
+	userAvg = ((avgForIndividual * countForIndividual) - actualScore) / (countForIndividual - 1)
+	for i in range(len(userScores)):
+		row = userScores[i]
+		if row[second] != 99:
+			theirAvg = allAvg[i]
+			finSet = allSets[first].intersection(allSets[i])
+			finSet.remove(second)
+			sim = pearson(userRow, row, userAvg, theirAvg, finSet)
+			simList.append((sim, row[second]))
+	thisSort = sorted(simList, key=lambda tup: abs(tup[0]), reverse = True)
+	for i in range(knnNum):
+		kTot += abs(thisSort[i][0])
+		tot += thisSort[i][0] * thisSort[i][1]
+	userScores[first][second] = actualScore
+	return tot/kTot
+
+
 def main():
 	#reading the data in
+	if len(sys.argv) < 4:
+		print("Usage: python EvaluateCFRandom.py method size repeats [knnNeighbors]")
+		print("Methods: meanUser meanItem weightedUser weightedItem knnUser knnItem")
+		print("knnUser and knnItem must have the additional knnNeighbors flag to indicate the number of neighbors to use")
+		return
 	method = sys.argv[1]
 	size = int(sys.argv[2])
 	repeats = int(sys.argv[3])
+	knnNum = -1
+	if(method == "knnUser" or method == "knnItem"):
+		knnNum = int(sys.argv[4])
 
 	f = open("jester-data-1.csv", "r")
+	f2 = open("output.csv", "w")
 
 	userScores = np.zeros((24983, 100))
 	itemsRatedPerUser = [0 for i in range(24983)]
@@ -100,18 +133,21 @@ def main():
 				pred = meanUser(userScores, avgPerItem[tup[1]], itemsRatedPerItem[tup[1]], tup[0], tup[1])
 				MAE += abs(actual - pred)
 				count += 1
+				f2.write(str(tup[0]) + ","  + str(tup[1]) + "," + str(actual) + ","  + str(pred) + "," + str(actual - pred) +"\n")
 		if method == "meanItem":
 			for tup in tupSet:
 				actual = itemScores[tup[1]][tup[0]]
 				pred = meanUser(itemScores, avgPerUser[tup[0]], itemsRatedPerUser[tup[0]], tup[1], tup[0])
 				MAE += abs(actual - pred)
-				count += 1	
+				count += 1
+				f2.write(str(tup[0]) + ","  + str(tup[1]) + "," + str(actual) + ","  + str(pred) + "," + str(actual - pred) +"\n")
 		if method == "weightedUser":
 			for tup in tupSet:
 				actual = userScores[tup[0]][tup[1]]
 				pred = weightedUser(userScores, avgPerUser[tup[0]], itemsRatedPerUser[tup[0]], tup[0], tup[1],
 					avgPerUser, itemsRatedPerUser, userSet)
 				MAE += abs(actual - pred)
+				f2.write(str(tup[0]) + ","  + str(tup[1]) + "," + str(actual) + ","  + str(pred) + "," + str(actual - pred) +"\n")
 				count += 1
 		if method == "weightedItem":
 			for tup in tupSet:
@@ -120,13 +156,30 @@ def main():
 					avgPerItem, itemsRatedPerItem, itemSet)
 				MAE += abs(actual - pred)
 				count += 1
-
+				f2.write(str(tup[0]) + ","  + str(tup[1]) + "," + str(actual) + ","  + str(pred) + "," + str(actual - pred) +"\n")
+		if method == "knnUser":
+			for tup in tupSet:
+				actual = userScores[tup[0]][tup[1]]
+				pred = knnUser(userScores, avgPerUser[tup[0]], itemsRatedPerUser[tup[0]], tup[0], tup[1],
+					avgPerUser, itemsRatedPerUser, userSet, knnNum)
+				MAE += abs(actual - pred)
+				f2.write(str(tup[0]) + ","  + str(tup[1]) + "," + str(actual) + ","  + str(pred) + "," + str(actual - pred) +"\n")
+				count += 1
+		if method == "knnItem":
+			for tup in tupSet:
+				actual = itemScores[tup[1]][tup[0]]
+				pred = knnUser(itemScores, avgPerItem[tup[1]], itemsRatedPerItem[tup[1]], tup[1], tup[0],
+					avgPerItem, itemsRatedPerItem, itemSet, knnNum)
+				MAE += abs(actual - pred)
+				f2.write(str(tup[0]) + ","  + str(tup[1]) + "," + str(actual) + ","  + str(pred) + "," + str(actual - pred) +"\n")
+				count += 1
 		MAEList.append(MAE/count)
-		print("Iteration ",i + 1," MAE: ",MAE/count)
-	print("Mean of all MAE's is ",s.mean(MAEList))
+		f2.write("Iteration " + str(i + 1) + " MAE: " + str(MAE/count))
+		f2.write("\n\n")
+	f2.write("Mean of all MAE's is " + str(s.mean(MAEList)) + "\n")
 	if repeats > 1:
-		print("Standard deviation of all MAE's is ",s.stdev(MAEList))
-	print("final is ",time.clock() - start)
+		f2.write("Standard deviation of all MAE's is " + str(s.stdev(MAEList)))
+	print("final time is ",time.clock() - start)
 
 
 
